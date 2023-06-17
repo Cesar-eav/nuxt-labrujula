e3096beb34495fedd547c41c39622a8c68aad6a6
Cesar-eav <cesar.eav@hotmail.es>
2023-06-16 22:13:33 -0400

    Api URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,14 +65,14 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "http://127.0.0.1:8000/",
+    baseURL: "http://elpionerodv.cl/",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
   env: {
-    baseURL: process.env.BASE_URL || 'http://127.0.0.1:8000'
+    baseURL: process.env.BASE_URL || 'http://elpionerodv.cl'
  
 }
 
