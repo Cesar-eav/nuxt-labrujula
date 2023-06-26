56c7b322f21d167d42636b4b053cd7f20845fbfa
Cesar-eav <cesar.eav@hotmail.es>
2023-06-26 12:32:04 -0400

    403 configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'nuxt-brujula4s',
+            script: './start.js',
+            env: {
+                HOST: 'localhost',
+                PORT: 3000
+            }
+        }
+    ],
+}
