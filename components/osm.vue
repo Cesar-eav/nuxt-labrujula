d70c0c1eeeb4d44c486137959a47011e804870f8
Cesar-eav <cesar.eav@hotmail.es>
2023-11-05 10:01:27 -0300

    Linea funciona

--- components/osm.vue
+++ components/osm.vue
@@ -1,15 +1,8 @@
 <template>
   <div>
-    <div>
-      <p v-if="hasGeolocationPermission">GPS: Si</p>
-      <p v-else>GPS: No</p>
-      <p v-if="latitude">GPS: {{ latitude }}, {{ longitude }}</p>
-      <p>WAYPOINTS
-        {{ waypoints }}</p>
-    </div>
     <div id="map-wrap" style="height: 80vh">
       <ClientOnly>
-        <l-map ref="map" :zoom="15" :center="[routeParams.lat, routeParams.lon]" @ready="onMapReady">
+        <l-map v-if="this.waypoints.length === 2" ref="map" :zoom="15" :center="[routeParams.lat, routeParams.lon]" @ready="onMapReady">
           <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
           <l-circle-marker :lat-lng="[routeParams.lat, routeParams.lon]" :radius="20" :weight="3"></l-circle-marker>
           <l-polyline v-if="routeLine" :lat-lngs="routeLine"></l-polyline>
@@ -42,12 +35,8 @@
 </template>
 
 <script>
-// import { LMap, LTileLayer } from 'vue2-leaflet';
 
 import LRoutingMachineControl from "nuxt-leaflet";
-
-//import { point, distance, lineString, featureCollection } from "@turf/turf";
-
 
 export default {
   layout: "Default",
@@ -57,13 +46,8 @@ export default {
       isClient: false,
       zoom: 13,
       tileLayerUrl: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      //center: [this.lat, this.lon], // Valor inicial, se actualizará con la posición actual del usuario
       userLocation: null, // La posición actual del usuario
-      waypoints: [
-        // [-33.032527, -71.630758], // Plaza Sotomayor
-        // [-33.4757888,  -70.6445312], // Muelle Barón
-      ],
-      waypoints2: [],
+      waypoints: [ ],
       latitude: null,
       longitude: null,
       hasGeolocationPermission: false,
@@ -80,7 +64,7 @@ export default {
   },
 
   methods: {
-    comprobacion() {
+     gpsPermission() {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
@@ -90,8 +74,17 @@ export default {
               position.coords.latitude,
               position.coords.longitude,
             ];
-            //this.center = this.userLocation; // Actualiza el centro del mapa con la posición del usuario
-            this.updateWaypoints(); // Actualiza los waypoints después de obtener la ubicación del usuario
+    
+                this.waypoints = [
+
+                  [parseFloat(this.routeParams.lat), parseFloat(this.routeParams.lon)], // URL PUNTO LABRUJULA 
+                  [this.latitude, this.longitude] // GPS
+                
+                ];
+       
+
+                this.onMapReady();
+
           },
           (error) => {
             this.hasGeolocationPermission = false;
@@ -103,7 +96,7 @@ export default {
       }
     },
 
-    async getCurrentPositionA() {
+    async getCurrentPosition() {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
@@ -113,28 +106,19 @@ export default {
             this.longitude = position.coords.longitude;
             console.log(`Latitude GPS: ${this.latitude}, Longitude: ${this.longitude}`);
 
-            // Verifica si this.routeParams no es null antes de acceder a sus propiedades
-            if (this.userLocation && this.userLocation.geometry?.coordinates) {
-              // Utiliza la función point de Turf.js para crear un punto geográfico con las coordenadas del punto de destino
-              this.destinationPoint = point([this.routeParams.lon, this.routeParams.lat]);
 
-              // Utiliza la función point de Turf.js para crear puntos geográficos con las coordenadas de inicio y destino
-              const startPoint = point([this.longitude, this.latitude]);
-              const endPoint = point([this.routeParams.lon, this.routeParams.lat]);
-              console.log("START", startPoint);
+            setInterval(() => {
+                this.waypoints = [
 
-              // Utiliza la función lineString de Turf.js para crear una línea que conecte los puntos de inicio y destino
-              this.routeLine = lineString([
-                startPoint.geometry.coordinates,
-                endPoint.geometry.coordinates,
-              ]);
+                  [parseFloat(this.routeParams.lat), parseFloat(this.routeParams.lon)], // URL PUNTO LABRUJULA 
+                  [this.latitude, this.longitude] // GPS
 
-              // Agrega la línea al mapa
-              if (this.isClient) {
-                const map = this.$refs.map.mapObject;
-                L.geoJSON(this.routeLine).addTo(map);
-              }
-            }
+             
+                ];
+                },100);
+                console.log("[parseFloat(this.routeParams.lat), parseFloat(this.routeParams.lon)]",[parseFloat(this.routeParams.lat), parseFloat(this.routeParams.lon)]);
+                console.log("[this.latitude, this.longitude]",[this.latitude, this.longitude]);
+                
           },
 
           (error) => {
@@ -145,30 +129,6 @@ export default {
       } else {
         console.log("Geolocation is not supported by this browser.");
       }
-    },
-
-    updateWaypoints() {
-      // Verifica si userLocation y center tienen valores antes de definir los waypoints
-      if (this.userLocation && this.latitude && this.longitude) {
-
-        this.waypoints2 = [
-
-          L.latLng(this.routeParams.lat, this.routeParams.lon), // Punto de inicio (latitud y longitud del primer marcador)
-          L.latLng(this.userLocation[0], this.userLocation[1]), // Punto de destino (misma ubicación del usuario)
-        ];
-
-      }
-
-      // if (this.userLocation) {   //SI ESPERO LA COMPROBACION NO FUNCIONA
-        this.waypoints = [
-
-          [this.routeParams.lat, this.routeParams.lon], // PUNTO LABRUJULA 
-          // [-33.037773, -71.634158]
-          [this.latitude, this.longitude]
-         
-         ];
-
-
     },
 
 
@@ -185,6 +145,8 @@ export default {
         }).addTo(map);
       }
     },
+
+
   },
 
   mounted() {
@@ -192,14 +154,9 @@ export default {
     this.isClient = process.client; // Actualiza el valor de isClient con el lado del cliente
 
     if (this.isClient) {
-      this.comprobacion();
-      this.getCurrentPositionA();
-      // this.actualizar()
+      this.getCurrentPosition();
 
     }
-
-    this.updateWaypoints();
-
   },
 
 
